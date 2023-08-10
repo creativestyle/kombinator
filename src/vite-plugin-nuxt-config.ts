961329004a3d7f32a43e82081f8834e97df39f3c
Krzysztof Daniel <krzysztof.daniel@creativestyle.pl>
2023-08-10 11:17:59 +0200

    Add option to pass config content

--- src/vite-plugin-nuxt-config.ts
+++ src/vite-plugin-nuxt-config.ts
@@ -2,30 +2,29 @@ import { Plugin } from 'vite';
 import fs from 'fs';
 import path from 'path';
 
-function createNuxtConfigFile(directory: string) {
-  const nuxtConfigTemplate = `// nuxt.config.ts
-export default {
-  export default defineNuxtConfig({})
-}
+function createNuxtConfigFile(directory: string, nuxtConfigTemplate?: string) {
+  const defaultTemplate = `// nuxt.config.ts
+export default defineNuxtConfig({})
 `;
 
+  const content = nuxtConfigTemplate || defaultTemplate;
   const filePath = path.resolve(directory, 'nuxt.config.ts');
 
   if (!fs.existsSync(filePath)) {
-    fs.writeFileSync(filePath, nuxtConfigTemplate, 'utf8');
+    fs.writeFileSync(filePath, content, 'utf8');
     console.log(`nuxt.config.ts created at: ${filePath}`);
   } else {
     console.log(`nuxt.config.ts already exists at: ${filePath}`);
   }
 }
 
-export default function NuxtConfigPlugin(directory: string): Plugin {
+export default function NuxtConfigPlugin(options: { directory: string, nuxtConfigTemplate?: string }): Plugin {
   return {
     name: 'vite-plugin-nuxt-config',
     apply: 'build',
     configResolved(resolvedConfig) {
       if (resolvedConfig.command === 'build') {
-        createNuxtConfigFile(directory);
+        createNuxtConfigFile(options.directory, options.nuxtConfigTemplate);
       }
     },
   };
